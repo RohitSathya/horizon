8e58282e47a267f41e08df6d1c149afeaf78b1eb
RohitSathya <156504207+RohitSathya@users.noreply.github.com>
2024-12-08 11:52:26 +0530

    Update index.js

--- back/index.js
+++ back/index.js
@@ -11,34 +11,15 @@ const app = express();
 // Middleware
 app.use(express.json());
 
-// Configure CORS
-const allowedOrigins = [
-  'https://horizon-ten-iota.vercel.app', // Frontend URL
-  'https://explorepricing.com',
-  'https://www.explorepricing.com',
-  'https://ecomerce-mern-royo.vercel.app',
-  'https://ecomerce-1-2b9c.vercel.app'
-];
-
+// Configure CORS to allow all origins
 const corsOptions = {
-  origin: function (origin, callback) {
-    // Allow requests with no origin (e.g., Postman or server-to-server requests)
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true); // Allow the request
-    } else {
-      callback(new Error('Not allowed by CORS')); // Block the request
-    }
-  },
+  origin: '*', // Allow all origins
   methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allowed HTTP methods
   allowedHeaders: ['Content-Type', 'Authorization'], // Allowed headers
-  credentials: true, // Allow cookies and credentials
 };
 
 // Use CORS middleware with the specified options
 app.use(cors(corsOptions));
-
-// Handle preflight requests for all routes
-app.options('*', cors(corsOptions));
 
 // Routes
 app.use('/api/auth', authRoutes);
@@ -51,7 +32,7 @@ mongoose.connect(process.env.MONGODB_URI, {
   .then(() => {
     console.log('Connected to MongoDB');
     const PORT = process.env.PORT || 5000;
-    app.listen(8080, () => {
+    app.listen(PORT, () => {
       console.log(`Server running on port ${PORT}`);
     });
   })
